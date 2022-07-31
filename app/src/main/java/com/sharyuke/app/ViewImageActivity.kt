6a63cb89b52dc3218bc2fd1eec00415b2ae3ed70
sharyuke <sharyuke@sharyuke.com>
2022-07-31 19:58:53 +0800

    图片滑块属性。

--- app/src/main/java/com/sharyuke/app/ViewImageActivity.kt
+++ app/src/main/java/com/sharyuke/app/ViewImageActivity.kt
@@ -1,10 +1,12 @@
 package com.sharyuke.app
 
 import android.os.Bundle
+import android.view.View
 import android.view.ViewStub
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.utils.widget.ImageFilterView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.slider.Slider
 import com.sharyuke.utils.adapterCreate
 import com.sharyuke.utils.loadUrl
 
@@ -17,14 +19,37 @@ class ViewImageActivity : AppCompatActivity() {
         title = "图片"
         val list = listOf(
             ImageModel("原图", R.layout.item_image_0, R.id.item_image_0),
-            ImageModel("全圆角", R.layout.item_image_1, R.id.item_image_1),
+            ImageModel("圆角", R.layout.item_image_1, R.id.item_image_1),
             ImageModel("对比度", R.layout.item_image_2, R.id.item_image_2),
             ImageModel("亮度", R.layout.item_image_3, R.id.item_image_3),
+            ImageModel("饱和度", R.layout.item_image_4, R.id.item_image_4),
         )
         findViewById<RecyclerView>(R.id.view_pic_rv).adapter = adapterCreate(R.layout.item_pic, list) {
             onHasItem {
-                getView<ViewStub>(R.id.item_view_sub).inflateAndFindView<ImageFilterView>(layout, id).loadUrl(DEMO_IMAGE_URL)
-                setText(R.id.item_name, name)
+                val imageView = getView<ViewStub>(R.id.item_view_sub).inflateAndFindView<ImageFilterView>(layout, id)
+                imageView.loadUrl(DEMO_IMAGE_URL)
+                val slider = getView<Slider>(R.id.item_slider)
+                slider.valueFrom = 0f
+                slider.visibility = View.VISIBLE
+                setText(R.id.item_name, String.format("%s：%.2f", name,
+                    when (adapterPosition) {
+                        0 -> 0f.apply { slider.visibility = View.GONE }
+                        1 -> slider.apply { valueTo = 1f }.let { 1f }
+                        2 -> slider.apply { valueTo = 4f }.let { 0.5f }
+                        3 -> slider.apply { valueTo = 3f }.let { 0.5f }
+                        4 -> slider.apply { valueTo = 4f }.let { 0.5f }
+                        else -> 0f
+                    }.apply { slider.value = this })
+                )
+                slider.addOnChangeListener { _, value, _ ->
+                    when (adapterPosition) {
+                        1 -> imageView.roundPercent = value
+                        2 -> imageView.contrast = value
+                        3 -> imageView.brightness = value
+                        4 -> imageView.saturation = value
+                    }
+                    setText(R.id.item_name, String.format("%s：%.2f", name, value))
+                }
             }
         }
     }
