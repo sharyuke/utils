0c03128324b2dc647c6d2f527bd22ec4a98b382c
sharyuke <sharuke@sharyuke.com>
2022-08-19 17:41:11 +0800

    Sql.

--- buildSrc/src/main/kotlin/Dependencises.kt
+++ buildSrc/src/main/kotlin/Dependencises.kt
@@ -7,3 +7,13 @@ const val glide = "com.github.bumptech.glide:glide:4.13.2"// google() , mavenCen
 
 //
 const val flexLayoutManager = "com.google.android.flexbox:flexbox:3.0.0"// Android官方流式布局 https://github.com/google/flexbox-layout
+
+// database
+const val roomRuntime = "androidx.room:room-runtime:2.4.3"
+const val roomKtx = "androidx.room:room-ktx:2.4.3"
+const val aptRoomCompile = "androidx.room:room-compiler:2.4.3"
+
+// coroutine
+const val coroutineAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1"
+const val coroutineCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1"
+const val lifeCycleScope = "androidx.lifecycle:lifecycle-runtime-ktx:2.4.0" // lifecycleScope
